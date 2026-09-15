730b3a3fec63e8f4607e4cd14131da6d6c018ec5
Lukas Bostelmann-Arp <L.Bostelmann@hotmail.com>
2026-09-15 11:53:22 +0200

    Added support for robot animation

--- robots_2d/main.py
+++ robots_2d/main.py
@@ -1,6 +1,7 @@
+import numpy as np
 from robots.robot_2r import Robot2R
 
-from robots_2d.tools.visualization import visualize
+from robots_2d.tools.visualization import animate_robot
 
 if __name__ == "__main__":
 
@@ -9,22 +10,46 @@ if __name__ == "__main__":
         l2 = 0.75,
     )
 
-    # Compute Inverse Kinematics
-    # robot.move_joints(
-    #     *robot.inverse_kinematics(
-    #         1.0,                    # x-coordinate
-    #         1.0                     # y-coordinate
-    #     )
-    # )
+    # Inverse Kinematics using analytical solution
+    if False:
+        qs = np.array(
+            robot.inverse_kinematics(
+                1.0,                    # x-coordinate
+                1.0                     # y-coordinate
+            )
+        )[np.newaxis, :]
 
-    robot.move_joints(
-        *robot.inverse_kinematics_num(
-            1.0,                    # x-coordinate
-            1.2                     # y-coordinate
+        animate_robot(
+            robot,
+            qs,
+            me = True
         )
-    )
 
-    visualize(
-        robot,
-        me = True
-    )
+    # Inverse Kinematics using numerical solution
+    if False:
+        qs = np.array(
+            robot.inverse_kinematics_num(
+                1.0,                    # x-coordinate
+                1.0                     # y-coordinate
+            )
+        )[np.newaxis, :]
+
+        animate_robot(
+            robot,
+            qs,
+            me = True
+        )
+
+    # Animate robot movement
+    if True:
+        q1s = np.linspace(0.0, 2 * np.pi, 250)
+        q2s = np.linspace(0.2, -4 * np.pi, 250)
+
+        qs = np.column_stack((q1s, q2s))
+
+        animate_robot(
+            robot,
+            qs,
+            me = True,
+            interval = 50
+        )
--- robots_2d/tools/visualization.py
+++ robots_2d/tools/visualization.py
@@ -1,105 +1,189 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.patches import Ellipse
+from matplotlib.animation import FuncAnimation
+from matplotlib.patches import Circle, Ellipse
 
 from robots_2d.classes.joint import JointType
 from robots_2d.classes.robot import Robot
 
 
-def visualize(robot: Robot, me: bool = False):
+def animate_robot(robot: Robot, qs: np.ndarray, me: bool = False, interval: int = 50):
 
-    poses = robot.forward_kinematics()
+    # ------------------------------------------------------------
+    # Create the figure
+    # ------------------------------------------------------------
 
     _, ax = plt.subplots()
 
-    # Plot all links
+    # Set the visible area based on the length of the robot arm
+    total_length = 0
+    for link in robot.links:
+        total_length += link.length
+
+    ax.set_xlim(-total_length * 1.5, total_length * 1.5)
+    ax.set_ylim(-total_length * 1.5, total_length * 1.5)
+
+    ax.axis("off")
+    ax.set_aspect("equal", adjustable="box")
+
+    # ------------------------------------------------------------
+    # Create artists once
+    # ------------------------------------------------------------
+
+    # Link lines
+    link_lines = {}
+
     for link in robot.links:
 
-        # Transformation matrix
-        T = poses[link.name]
-
-        # Change reference frame for start and end point
-        p_start = T * np.array([0.0, 0.0, 0.0])
-        p_end = T * np.array([link.length, 0.0, 0.0])
-
-        # Draw the end-effector
         if link.name == "link_end":
+            continue
 
-            # Plot a circle
-                circle = plt.Circle(
-                    (p_start[0], p_start[1]),   # center coordinates
-                    radius = 0.075,             # Radius 
-                    color = 'green',            # Color
-                    fill = True,                # Fill circle
-                    zorder = 2                  # Draw in front
-                )
-                ax.add_patch(circle)
+        line, = ax.plot(
+            [],
+            [],
+            linewidth = 5,
+            color = "black",
+            zorder = 1,
+        )
 
-        # Draw a normal link
-        else:
+        link_lines[link.name] = line
 
-            # Plot a line
-            ax.plot(
-                [p_start[0], p_end[0]],     # xs
-                [p_start[1], p_end[1]],     # ys
-                linewidth = 5,              # Line width
-                color = 'black',            # Color
-                zorder = 1                  # Draw in the back
-            )
+    # Joint circles
+    joint_circles = {}
 
-    # Plot all joints
     for joint in robot.joints:
 
-        # Revolute joints
         if joint.type == JointType.REVOLUTE:
 
-            # Transformation matrix
-            T = poses[joint.child.name]
-
-            # Change reference frame for joint origin
-            p_joint = T * np.array([0.0, 0.0, 0.0])
-
-            # Plot a circle
-            circle = plt.Circle(
-                (p_joint[0], p_joint[1]),   # center coordinates
-                radius = 0.075,             # Radius 
-                color = 'red',              # Color
-                fill = True,                # Fill circle
-                zorder = 2                  # Draw in front
+            circle = Circle(
+                (0, 0),
+                radius = 0.075,
+                color = "red",
+                fill = True,
+                zorder = 2,
             )
+
             ax.add_patch(circle)
+            joint_circles[joint.child.name] = circle
 
         elif joint.type == JointType.FIXED:
-
-            # Don't draw fixed joints
             pass
 
         else:
             raise RuntimeError(
-                f"Plotting of joint type {joint.type} no implemented yet!"    
+                f"Plotting of joint type {joint.type} not implemented yet!"
             )
 
-    # Plot manipulability ellipse
+    # End-effector circle
+    end_effector = Circle(
+        (0, 0),
+        radius = 0.075,
+        color = "green",
+        fill = True,
+        zorder = 2,
+    )
+
+    ax.add_patch(end_effector)
+
+    # Manipulability ellipse
     if me:
-        center, width, height, angle = robot.manipulability_ellipse()
-
         ellipse = Ellipse(
-            center,              
-            width = width,
-            height = height,
-            angle = angle,
-            color = 'green',
+            (0, 0),
+            width = 0,
+            height = 0,
+            angle = 0,
+            color = "green",
             fill = False,
-            zorder = 2
+            zorder = 2,
         )
-        ax.add_patch(ellipse)
-         
 
-    # Adjust plotting parameters
-    ax.axis("off")
-    ax.set_aspect(
-        'equal',
-        adjustable='box'
+        ax.add_patch(ellipse)
+
+    # ------------------------------------------------------------
+    # Update function
+    # ------------------------------------------------------------
+
+    def update(frame):
+
+        q = qs[frame]
+
+        # Set robot configuration
+        robot.move_joints(*q)
+        poses = robot.forward_kinematics()
+
+        # Links
+        for link in robot.links:
+
+            if link.name == "link_end":
+                continue
+
+            T = poses[link.name]
+
+            p_start = T * np.array([0.0, 0.0, 0.0])
+            p_end = T * np.array([link.length, 0.0, 0.0])
+
+            link_lines[link.name].set_data(
+                [p_start[0], p_end[0]],
+                [p_start[1], p_end[1]],
+            )
+
+        # Joints
+        for joint in robot.joints:
+
+            if joint.type != JointType.REVOLUTE:
+                continue
+
+            T = poses[joint.child.name]
+
+            p_joint = T * np.array([0.0, 0.0, 0.0])
+
+            joint_circles[joint.child.name].center = (
+                p_joint[0],
+                p_joint[1],
+            )
+
+        # End effector
+        T = poses["link_end"]
+
+        p_end_effector = T * np.array([0.0, 0.0, 0.0])
+
+        end_effector.center = (
+            p_end_effector[0],
+            p_end_effector[1],
+        )
+
+        # Manipulability ellipse
+        if me:
+
+            center, width, height, angle = (
+                robot.manipulability_ellipse()
+            )
+
+            ellipse.center = center
+            ellipse.width = width
+            ellipse.height = height
+            ellipse.angle = angle
+
+        return (
+            list(link_lines.values())
+            + list(joint_circles.values())
+            + [end_effector]
+            + ([ellipse] if me else [])
+        )
+
+    # ------------------------------------------------------------
+    # Animation
+    # ------------------------------------------------------------
+
+    animation = FuncAnimation(
+        ax.figure,
+        update,
+        frames = len(qs),
+        interval = interval,
+        blit = True,
+        repeat = True,
     )
 
     plt.show()
+
+    return animation
